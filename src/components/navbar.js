dfdd1cbfb15595863da0c1a8d6e3d96eea8c463c
Ghanshyam Prajapati <63035436+Ghanshyam89@users.noreply.github.com>
2024-02-05 11:56:23 +0000

    Corrected

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,15 +4,15 @@ import { Link, useLocation } from "react-router-dom";
 function Navbar() {
     const location = useLocation();
 
-    const isHomePage = location.pathname === "/";
+    const isHomePage = location.pathname === "/BlogPostApp";
 
     return (
         <nav className="navbar navbar-dark bg-dark">
-            <Link to="/" className="navbar-brand mb-0 h1">BLOGPOST</Link>
+            <Link to="/BlogPostApp" className="navbar-brand mb-0 h1">BLOGPOST</Link>
             { isHomePage ? (
-                <Link to="/add" className="btn btn-dark navbar-brand mb-0 h1">Add Post</Link>
+                <Link to="/BlogPostApp/add" className="btn btn-dark navbar-brand mb-0 h1">Add Post</Link>
             ) : (
-                <Link to="/" className="btn btn-dark navbar-brand mb-0 h1">Home</Link>
+                <Link to="/BlogPostApp/" className="btn btn-dark navbar-brand mb-0 h1">Home</Link>
             )}
         </nav>
     );
